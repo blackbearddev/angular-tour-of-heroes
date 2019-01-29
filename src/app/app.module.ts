cbf6a6afc91012de5b03c7758029c9a9145a0731
BlackBeard <c@c.com>
2019-01-29 16:42:22 -0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 //import form para habilitar ngModule
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -9,10 +10,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
-]
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +25,10 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,16 +3,84 @@ import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+//error handling rxjs
+import { catchError, map, tap} from 'rxjs/operators';
+
+
+const httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
 
 @Injectable({
   providedIn: 'root'
 })
+  
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+
+  constructor(private http: HttpClient, 
+              private messageService: MessageService) { }
 
   getHeroes() : Observable<Hero[]>{
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('Fetched Heroes')),
+      catchError(this.handleError('getHeroes', []))
+    );
+    //return of(HEROES);
+  }
+
+  getHero(id: number) : Observable<Hero>{
+    //this.messageService.add(`HeroService : fetched hero id=${id}`);
+    //return of(HEROES.find(hero=>hero.id===id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fecthed hero id=${id}`)),
+      catchError(this.handleError<Hero>(`gerHero id=${id}`))
+    );
+  } 
+
+  updateHero(hero: Hero): Observable<any>{
+    
+  
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+  
+  addHero(hero: Hero) : Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+        tap((hero: Hero) => this.log(`added Hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      )
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero>{
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  private log(message: string)
+  {
+    this.messageService.add(`HeroService : ${message}`);
+  }
+
+  private handleError<T>(operation='operation', result?: T){
+    return (error:any): Observable<T> => {
+      console.log(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
